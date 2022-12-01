c6a0c866ddcace3845bac0434192bb59c1738373
korshunX <gourment@gmx.net>
2022-12-01 19:51:27 +0100

    added solution 3 to "code" .. a little php

---
+++ code/003_a_little_php.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+    simple code for evaluation again
+    because I don't have a working server installation at the moment, 
+    I used an online tool:
+    
+    https://onlinephp.io/
+
+    easy .. letZ go!
+*/
+
+$x = 72311;
+$y = 89525;
+$z = '=';
+eval("\$k$z\$x^\$y;");
+echo $k;
